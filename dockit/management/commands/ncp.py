7f1f83b4dbff295c767bcbd74a0c8f08a1d9bc3b
druuu <dinesh@micropyramid.com>
2017-05-08 09:46:00 +0530

    upgrade django 1.11.1, basecommand change option list

--- dockit/management/commands/ncp.py
+++ dockit/management/commands/ncp.py
@@ -6,16 +6,17 @@ from django.http import HttpResponseForbidden
 from dockit.models import User, Container
 
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (make_option('--container_id', action='store', \
-            dest='container_id'), make_option('--sessionid', action='store', dest='sessionid'))
+    def add_arguments(self, parser):
+        parser.add_argument('container_id', nargs='+', type=str)
+        parser.add_argument('sessionid', nargs='+', type=str)
 
     def handle(self, *args, **options):
         try:
-            sessionid = options['sessionid'].rstrip()
+            sessionid = options['sessionid'][0].rstrip()
             session = Session.objects.get(session_key=str(sessionid))
             uid = session.get_decoded().get('_auth_user_id')
             user = User.objects.get(id=uid)
-            container = Container.objects.get(container_id=options['container_id'])
+            container = Container.objects.get(container_id=options['container_id'][0])
         except ObjectDoesNotExist:
             #raise HttpResponseForbidden
             pass
